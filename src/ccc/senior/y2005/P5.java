09604d1476cd07338c4d8d589b427beef84f424c
sunny <eoflndafljn>
2016-01-09 11:40:40 -0500

    began wrking on ccc 2005 s5

---
+++ src/ccc/senior/y2005/P5.java
@@ -0,0 +1,14 @@
+package ccc.senior.y2005;
+
+import java.util.Scanner;
+
+public class P5 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		
+		s.close();
+	}
+
+}
