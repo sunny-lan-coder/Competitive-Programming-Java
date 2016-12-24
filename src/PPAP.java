83c1750bb3a186ecc134f28ea16c5b2b4a30eeb5
sunny <sunny.lan.coder@gmail.com>
2016-12-23 22:41:09 -0500

    lol

---
+++ src/PPAP.java
@@ -0,0 +1,29 @@
+
+public class PPAP {
+
+	public static void main(String[] args) {
+		String 苹果="苹果";
+		
+		String 钢笔="钢笔";
+		
+		String 菠萝="菠萝";
+		
+		System.out.println("我有一个"+钢笔);
+		System.out.println("我有一个"+苹果);
+		System.out.println("uh");
+		System.out.println(苹果+钢笔);
+		
+		System.out.println("我有一个"+钢笔);
+		System.out.println("我有一个"+菠萝);
+		System.out.println("uh");
+		System.out.println(菠萝+钢笔);
+		
+		System.out.println("苹果钢笔...");
+		System.out.println("菠萝钢笔...");
+		System.out.println("uh");
+		System.out.println(钢笔+菠萝+苹果+钢笔);
+		
+		System.out.println("bulluuulluu");
+	}
+
+}
